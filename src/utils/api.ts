2a52a6539d4c934cb7f40046aa423a69a1ae29c8
Ana Caroline <carolineignacior@gmail.com>
2022-06-19 20:26:52 -0300

    remove uneccessary slash

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { DatabaseInputParams } from './types';
 
-const GET_SCHEMA_PATH = '/api/database-schema';
+const GET_SCHEMA_PATH = 'api/database-schema';
 
 export const SCHEMA_GET = async (
   dbAccessParams: DatabaseInputParams,
 ): Promise<Object> => {
-  const url = `https://db-viewer-api.herokuapp.com/${GET_SCHEMA_PATH}`;
+  const url = `http://db-viewer-api.herokuapp.com/${GET_SCHEMA_PATH}`;
 
   const { data } = await axios.post(url, { dbAccessParams });
   return data;
